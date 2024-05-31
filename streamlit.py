32ac304a30f84fdc2969ca5cbf0535fd74652067
Ben080939 <54099395+Ben080939@users.noreply.github.com>
2024-06-01 04:03:58 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,9 +4,12 @@ st.header("open api key를 입력하세요.")
 
 apikey = st.text_input("API Key",key="api", type="password")
 
-if 'key' not in st.session_state:
- st.session_state['key'] = 'st.text_input("API Key",key="api", type="password")'
-
+if 'aws_key' not in st.session_state:
+    st.session_state = ['aws_key'] = 'apikey'
+	
+    or
+    
+    st.session_state.aws_key = 'apikey'
 st.divider() 
 
 st.header("무엇이든 물어보세요.")
