003e5a3fc62b8f0563323b69a547db9df58a74a0
Ben080939 <54099395+Ben080939@users.noreply.github.com>
2024-06-02 17:42:25 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -8,7 +8,7 @@ apikey = st.text_input("API Key를 입력하세요", type="password")
 @st.cache_data()
 def ask(pp):
     client = OpenAI(api_key = apikey)
-    response = openai.ChatCompletion.create(
+    response = client.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
@@ -21,7 +21,7 @@ pp = st.text_input("질문을 입력하세요")
 
 if st.button("실행"):
     client = OpenAI(api_key =apikey)
-    response = openai.ChatCompletion.create(
+    response = client.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
