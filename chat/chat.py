3af9946ff10cb9a6eea3843cfcf17182c89fce0a
Ben080939 <54099395+Ben080939@users.noreply.github.com>
2024-06-02 18:30:37 +0900

    Update chat.py

--- chat/chat.py
+++ chat/chat.py
@@ -52,8 +52,9 @@ if prompt := st.chat_input("What is up?"):
 	thread_messages = client.beta.threads.messages.list(thread.id)
 	for msg in thread_messages.data:
 	   response = f"Echo: {msg.content[0].text.value}"
-	
-	   st.session_state.messages.append({"role": "assistant", "content": response})
+	with st.chat_message("assistant"): 
+	     st.markdown(response)
+	st.session_state.messages.append({"role": "assistant", "content": response})
 
 	
 
