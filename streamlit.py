84c5804faedef9bdbc6b6fb3838a7ad4e636a106
Ben080939 <54099395+Ben080939@users.noreply.github.com>
2024-06-02 19:44:16 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,13 +2,16 @@ import streamlit as st
 from openai import OpenAI
 
 apikey = st.text_input("api key를 입력하세요", type="password", key="api") 
-st.session_state.api
 
 
+"st.session_state object:", st.session_state
+
+if 'key' not in st.session_state: 
+    st.session_state['key'] = apikey
 
 @st.cache_data()
 def ask(pp):
-    client = OpenAI(api_key= apikey)
+    client = OpenAI(api_key= st.session_state.key)
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
