1360743f002e6c92ce59edf0e3a29208d44cca3f
Ben080939 <54099395+Ben080939@users.noreply.github.com>
2024-06-02 18:53:36 +0900

    Update streamlit-image.py

--- pages/streamlit-image.py
+++ pages/streamlit-image.py
@@ -1,11 +1,23 @@
 import streamlit as st
+from openai import OpenAI
+
+apikey = st.text_input("api key를 입력하세요", type="password", key="api") 
+st.session_state.api
+
 
 st.header("무엇이든 그려보세요.")
-prompt2 = st.text_input("명령")
+prompti = st.text_input("명령")
+
+@st.cache_data()
+def draw(prompt):
+    client = OpenAI(api_key= apikey)
+    response = client.images.generate(model="dall-e-3",prompt=prompt)
+    image_url = response.data[0].url
+    image = f"![alt text]({image_url})"
+    return image
+  
+
 
 if st.button("start"):
-  from openai import OpenAI
-  client = OpenAI(api_key= apikey)
-  response = client.images.generate(model="dall-e-3",prompt=prompt2)
-  image_url = response.data[0].url
-  st.markdown(f"![alt text]({image_url})")
+  img = draw(prompti)
+  st.markdown(img)
