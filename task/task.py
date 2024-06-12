fcd5a5383ca6a3f01db6e7db0a6bded96babce90
Ben080939 <54099395+Ben080939@users.noreply.github.com>
2024-06-12 16:05:08 +0900

    Update task.py

--- task/task.py
+++ task/task.py
@@ -3,19 +3,16 @@ from openai import OpenAI
 
 apikey = st.text_input("api key를 입력하세요", type="password") 
 
-st.header("무엇이든 그려보세요.")
-prompti = st.text_input("명령")
+st.header("음식 메뉴 추천")
+prompti = st.text_input("키워드")
 
 @st.cache_data()
 def draw(prompt):
     client = OpenAI(api_key=apikey)
-    response = client.images.generate(model="dall-e-3",prompt=prompt)
+    response = client.images.generate(model="dall-e-3",prompt=f'{prompt}와 관련된 음식을 추천해줘')
     image_url = response.data[0].url
     image = f"![alt text]({image_url})"
     return image
-
-if 'prompt' not in st.session_state: 
-    st.session_state['prompt'] = prompti
     
 if st.button("start"):
   img = draw(prompti)
