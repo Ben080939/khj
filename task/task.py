c0c937713ea174ac8a1b71244684968909525574
Ben080939 <54099395+Ben080939@users.noreply.github.com>
2024-06-12 16:38:02 +0900

    Update task.py

--- task/task.py
+++ task/task.py
@@ -6,14 +6,27 @@ apikey = st.text_input("api key를 입력하세요", type="password")
 st.header("음식 메뉴 추천")
 prompti = st.text_input("키워드")
 
+
+response = client.chat.completions.create(
+  model="gpt-4o",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Who won the world series in 2020?"},
+    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+    {"role": "user", "content": "Where was it played?"}
+  ]
+)
+r = response.choices[0].message.content
+st.markdown(r)
+
 @st.cache_data()
 def draw(prompt):
     client = OpenAI(api_key=apikey)
-    response = client.images.generate(model="dall-e-3",prompt=f'{prompt}를 주제로 하는 음식 한가지를 한국어로')
+    response = client.images.generate(model="dall-e-3",prompt= prompt)
     image_url = response.data[0].url
     image = f"![alt text]({image_url})"
     return image
     
 if st.button("start"):
-  img = draw(prompti)
+  img = draw(r)
   st.markdown(img)
