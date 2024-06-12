358e3f6cedb70b0a8b248c0c95399d371e6abfc6
Ben080939 <54099395+Ben080939@users.noreply.github.com>
2024-06-12 15:59:44 +0900

    Create task.py

---
+++ task/task.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from openai import OpenAI
+
+apikey = st.text_input("api key를 입력하세요", type="password") 
+
+st.header("무엇이든 그려보세요.")
+prompti = st.text_input("명령")
+
+@st.cache_data()
+def draw(prompt):
+    client = OpenAI(api_key=apikey)
+    response = client.images.generate(model="dall-e-3",prompt=prompt)
+    image_url = response.data[0].url
+    image = f"![alt text]({image_url})"
+    return image
+
+if 'prompt' not in st.session_state: 
+    st.session_state['prompt'] = prompti
+    
+if st.button("start"):
+  img = draw(prompti)
+  st.markdown(img)
