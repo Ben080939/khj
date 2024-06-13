a1499b8719e1ac1b2a48fb44e1e130d67c79fd20
Ben080939 <54099395+Ben080939@users.noreply.github.com>
2024-06-13 16:22:26 +0900

    Update task.py

--- task/task.py
+++ task/task.py
@@ -27,6 +27,37 @@ if st.button("start"):
     img = draw(r)
     st.markdown(img)
 
+if st.button("맛집 추천"):
+    assistant = client.beta.assistants.create(
+      instructions="당신은 유능한 비서입니다.",
+      model="gpt-4-turbo-preview",
+      tools=[{"type": "file_search"}],
+      tool_resources={
+          "file_search":{
+              "vector_store_ids": [vector_store.id]
+          }
+      }
+    )
+    
+    thread = client.beta.threads.create(
+      messages=[
+        {
+          "role": "user",
+          "content": f'{r}을 하는 식당을 첨부된 파일에서 추천해줘',
+          "attachments": [{"file_id": message_file.id, "tools":[{"type":"file_search"}]]
+        }
+      ]
+    )
+    
+    run = client.beta.threads.runs.create_and_poll(
+        thread_id=thread.id,
+        assistant_id=assistant.id
+    )
+    
+    thread_messages = client.beta.threads.messages.list(thread.id, run_id=run.id)
+    
+    for msg in thread_messages.data:
+      st.markdown(f"{msg.role}: {msg.content[0].text.value}")
 
 
 
