41c1c28712f5795e601d47c9556abee4872a6660
Ben080939 <54099395+Ben080939@users.noreply.github.com>
2024-06-19 16:42:03 +0900

    Update task.py

--- task/task.py
+++ task/task.py
@@ -16,7 +16,7 @@ def download_and_save(url, filename):
   with open(filename,'w') as fo:
     fo.write(text)
 
-apikey = st.text_input("api key를 입력하세요", type="password") 
+apikey = st.text_input("api key를 입력하세요") 
 
 client = OpenAI(api_key=apikey)
 
